2c7f47b7c93a3137fb6bb29a73c3abe098e42a5a
LIRA <andersonmiguel2005xd@gmail.com>
2025-11-03 17:18:39 -0500

    controller

---
+++ src/main/java/com/tecsup/petclinic/webs/TypesController.java
@@ -0,0 +1,26 @@
+package com.tecsup.petclinic.webs;
+
+import com.tecsup.petclinic.entities.Type;
+import com.tecsup.petclinic.services.TypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class TypesController {
+
+    private final TypeService typeService;
+
+    public TypesController(TypeService typeService) {
+        this.typeService = typeService;
+    }
+
+    @GetMapping("/types")
+    public String listTypes(Model model) {
+        List<Type> types = typeService.findAll();
+        model.addAttribute("types", types);
+        return "types/index";
+    }
+}
